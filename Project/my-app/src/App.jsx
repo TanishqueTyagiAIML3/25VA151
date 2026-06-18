cb6a9d11ef0fdce4aec302cb9153db3d896f8532
Tanishque Tyagi <tanishque30@gmai.com>
2026-06-18 06:17:46 +0530

    commit

--- Project/my-app/src/App.jsx
+++ Project/my-app/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function Item(props){
   return(
     <div className="container">
-      <img src={props.image} alt="crupp" />
+      <img src={props.image} alt="crupp"/>
       <button  style={{backgroundColor:'blue'}}onClick={props.adding}>Add To Cart</button>
     </div>
   )
@@ -14,13 +14,18 @@ function Item(props){
 function App() {
   const [count, setCount] = useState(0)
   function add(){
+    const conf=confirm("Are you sure you want to add")
+    if(conf===true)
+    {
     setCount(count+1)
+    alert("The Product was succesfully added")
+    }
   }
   return (
     <>
     <main>
       <header>
-        <img src="https://m.media-amazon.com/images/G/31/social_share/amazon_logo._CB633266945_.png"  alt="" style={{width:'30px',height:'20px',margin:'10px'}} />
+        <img src="https://m.media-amazon.com/images/G/31/social_share/amazon_logo._CB633266945_.png"  alt="" style={{width:'40px',height:'30px',margin:'10px'}} />
         <a href="">Home</a>
         <select name="" id="" style={{height:'20px', marginTop:'10px'}}><option value="">Select Category</option><option value="" >Clothes</option><option value="">Shoes</option><option value="">Mobile Phones</option></select>
         <a href="">Location</a>
@@ -39,7 +44,25 @@ function App() {
         <Item image="https://n.nordstrommedia.com/id/sr3/ca66619f-9489-4b5c-b007-82a2948eb0f8.jpeg?h=365&w=240&dpr=2"  adding={add}></Item>
       </div>
       <footer>
-        <p>Copyright Reseverd by ABES Enginnering College</p>
+        <p style={{borderBottom:'2px solid grey'}}>Copyright Reseverd by ABES Enginnering College</p>
+        <div style={{display:'grid',gridTemplateColumns:'auto auto auto'}}>
+          <p><ul>ABout Clothes
+            <li>Clothes Is Just like a Fashion . Everyone have differnet aspect about clothes</li>
+            </ul> </p>
+          <p>
+            Contact Details
+            <ul>
+            <li>phone number: 9650191080</li>
+            <li>Email: tanishque30@gmai.com</li>
+            </ul>
+          </p>
+          <p>
+            News Letter Subscription 
+            <ul>
+              <li>tyagi07183@gmail.com</li>
+            </ul>
+          </p>
+        </div>
       </footer>
     </main>
     </>
